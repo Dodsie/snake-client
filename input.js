040d6a5e007556e5f958e748097c07aabca8b1bf
Dodsie <baileydods@gmail.com>
2022-03-16 17:38:57 -0600

    input & play

---
+++ input.js
@@ -0,0 +1,18 @@
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+
+};
+const setupInput = function(conn) {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+
+};
+setupInput();
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,26 +1,10 @@
 const connect = require("./client");
-const net = require("net");
+const setupInput = require("./input");
 
 
+console.log("Connecting ...");
+connect();
 
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
-    process.exit();
-  }
-
-};
-
-
-
-
-
-const setupInput = function(conn) {
-  const stdin = process.stdin;
-  stdin.on("data", handleUserInput);
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-
-};
 setupInput();
+
+
