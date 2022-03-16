50b9d46b0f6053d80ec051aeea7c303a15000cfd
Dodsie <baileydods@gmail.com>
2022-03-16 16:04:26 -0600

    update

--- client.js
+++ client.js
@@ -3,12 +3,18 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: '10.0.0.159',
+    host: '165.227.47.243',
     port: 50541,
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
   conn.on('connect', () => {
     console.log('Sucessfuly connected to game server');
     conn.write('Name: BCD');
